8d5d90040c12919cee2a11a9b293381182bba991
Dashamir Hoxha <dashohoxha@gmail.com>
2013-08-19 10:43:35 +0000

    Fixed testing doc and script.

--- test/import-docs.sh
+++ test/import-docs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-drush="drush --include=../modules/docbook/drush/"
+#drush="drush --include=../modules/docbook/drush/"
+drush="drush"
 
 cd $(dirname $0)
 
 $drush docbook2dbw $(pwd)/slackrouter_en.xml > slackrouter_en_1.xml
 $drush -u admin dbw-import $(pwd)/slackrouter_en_1.xml
 
-#$drush docbook2dbw $(pwd)/gateway_server_en.xml > gateway_server_en_1.xml
-#$drush -u admin dbw-import $(pwd)/gateway_server_en_1.xml
+$drush docbook2dbw $(pwd)/gateway_server_en.xml > gateway_server_en_1.xml
+$drush -u admin dbw-import $(pwd)/gateway_server_en_1.xml
 
